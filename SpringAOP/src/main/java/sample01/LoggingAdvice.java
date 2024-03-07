769a94207c165c643d75ff1391bb1cfd36dbf3cd
jjh319 <136777270+jjh319@users.noreply.github.com>
2024-03-07 10:29:19 +0900

    Spring AOP 처리시간 구하기

--- SpringAOP/src/main/java/sample01/LoggingAdvice.java
+++ SpringAOP/src/main/java/sample01/LoggingAdvice.java
@@ -2,6 +2,7 @@ package sample01;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.aop.aspectj.MethodInvocationProceedingJoinPoint;
+import org.springframework.util.StopWatch;
 
 public class LoggingAdvice {
 
@@ -15,14 +16,22 @@ public class LoggingAdvice {
 
     public void trace(ProceedingJoinPoint joinPoint) throws Throwable {
         // 앞에 삽입될 코드
-        System.out.println("하늘");
+//        System.out.println("하늘");
+        String methodName = joinPoint.getSignature().toShortString();
+        System.out.println("메소드 : " +methodName);
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
 
         // 핵심코드 메소드 호출
         joinPoint.proceed();
 
         // 뒤에 삽입될 코드
         System.out.println("땅");
-        
+
+        stopWatch.stop();
+        System.out.println("처리 시간 : " + stopWatch.getTotalTimeMillis() / 1000 + "초");
+
     } // trace
 
 } // end class
